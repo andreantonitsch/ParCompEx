5079fab0b3dd18570aaa490dd05ebc0be2a9537b
andre <andre.antonitsch@acad.pucrs.br>
2018-05-22 22:45:06 -0300

    slavemaster

--- slavemaster.c
+++ slavemaster.c
@@ -5,6 +5,10 @@
 #define TASKS = 7
 #define TASK_SIZE = 100
 
+#define DATA_TAG = 0
+#define DONE_TAG = 2
+
+
 int main(int argc, char **argv)
 {
 
@@ -15,6 +19,9 @@ int main(int argc, char **argv)
     int* task;     //working task
 
     int next_task;
+    int complete_tasks;
+
+    int sopranaomenar = 0;
 
     int* processes; //table for slaveid -> current_task
 
@@ -25,7 +32,7 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD,  &proc_n);  // pega informação do numero de processos (quantidade total)
     double start_time;
  
-    // receber da esquerda
+    // Inicializa todos os nodos
     if (my_rank == 0)
     {
         start_time = MPI_Wtime();
@@ -36,46 +43,94 @@ int main(int argc, char **argv)
         for(i = 1; i < TASKS; i++)
             bag[i] = bag[0] + i * TASK_SIZE;
 
-        processes = malloc(sizeof(int) * proc_n;
-        for(i = 0; i < proc_n; i++)
+        processes = malloc(sizeof(int) * proc_n);
+        for(i = 1; i < proc_n; i++)
             processes[i] = 0;
         
         next_task = 0;
+        complete_tasks = 0;
 
     } else
         task = malloc(sizeof(int) * TASK_SIZE);
 
-    int j;
-    for (j = 0; j < rows; j++){
-        if ( my_rank != 0 )    // sou o primeiro?
-            MPI_Recv(message, proc_n, MPI_INT, my_rank-1, 1, MPI_COMM_WORLD, &status); // não sou o primeiro, recebo da esquerda
 
-    // processo mensagem
-       int k;
-        for(k =0; k<rows; k++)
-            message[my_rank] = my_rank * j; // incremento um na mensagem recebida
-
-    // enviar para a direita
-        if ( my_rank == proc_n-1 ){ // sou o último?
-            int i = 0;
+    // Data flow
+    if (my_rank == 0)
+    {
+        //Primeira rajada
+        int n;
+        for(n=1; n<proc_n; n++)
+        {
+            processes[n] = next_task;
+            MPI_Send(bag[next_task], TASK_SIZE, MPI_INT, n, DATA_TAG, MPI_COMM_WORLD);
+            next_task++;
         }
-        else
-            MPI_Send(message, proc_n, MPI_INT, my_rank+1, 1, MPI_COMM_WORLD); // não sou o último, envio mensagem para a direita
-    
+
+        //isso ta certo????
+        while(next_task < TASKS)
+        {
+            MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            MPI_Recv(bag[processes[status.MPI_SOURCE]], TASK_SIZE, MPI_INT, MPI_ANY_SOURCE, DATA_TAG, MPI_COMM_WORLD, &status);
+            complete_tasks++;
+            processes[status.MPI_SOURCE] = next_task;
+            MPI_Send(bag[next_task], TASK_SIZE, MPI_INT, status.MPI_SOURCE, DATA_TAG, MPI_COMM_WORLD);
+            next_task++;
+        }
+
+        //Manda todo mundo parar a zoeira
+        //for(n=1; n<proc_n; n++)
+        //    MPI_Send(&sopranaomenar, 0, MPI_INT, n, 2, MPI_COMM_WORLD);
+
+        //Espera todo mundo parar de parar a zoeira. ordem?
+        while(complete_tasks < TASKS)
+        {
+            MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(bag[processes[status.MPI_SOURCE]], TASK_SIZE, MPI_INT, MPI_ANY_SOURCE, DATA_TAG, MPI_COMM_WORLD, &status);
+            complete_tasks++;
+
+            //manda parar a zoeira.
+            MPI_Send(&sopranaomenar, 1, MPI_INT, status.MPI_SOURCE, DONE_TAG, MPI_COMM_WORLD);
+        }
+
+
+
+    } else {
+
+        while(true)
+        {
+            MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            if(status.MPI_TAG == 0)
+            {
+                MPI_Recv(task, TASK_SIZE, MPI_INT, 0, DATA_TAG, MPI_COMM_WORLD, &status);
+                
+                //bubblesort
+                int k, l, m;
+                for(k=0;k<TASK_SIZE;k++){
+                    for(l=k;l<TASK_SIZE-1;l++){
+                        if(task[l] > task[l+1])
+                        {
+                            m = task[l];
+                            task[l] = task[l+1];
+                            task[l+1] = m;
+                        }
+                    }
+                }
+
+                MPI_Send(task, TASK_SIZE, MPI_INT, 0, DATA_TAG, MPI_COMM_WORLD);
+            }
+            
+            //recv para desbloquear master?
+            if(status.MPI_TAG == 2)
+                MPI_Recv(&sopranaomenar, 1, MPI_INT, 0, DONE_TAG, MPI_COMM_WORLD, &status);
+        }
+
     }
 
-    if(my_rank == proc_n -1){
-        int k =0;
-        MPI_Send(&k, 1, MPI_INT, 0, 2, MPI_COMM_WORLD); 
-    }
 
-    if(my_rank == 0){
-         int k;
-         MPI_Recv(&k, 1, MPI_INT, proc_n-1, 2, MPI_COMM_WORLD, &status);
-
-        double end_time = MPI_Wtime();
-        printf("time: %f\n", end_time - start_time);
-    }
+    double end_time = MPI_Wtime();
+    printf("time: %f\n", end_time - start_time);
 
     MPI_Finalize();
 
